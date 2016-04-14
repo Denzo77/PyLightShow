f6a722c6413c45272a327893d041534d3d8f4f6b
Denzo77 <denzo1@gmail.com>
2016-04-14 19:31:33 +0100

    more implementing sound stream input

--- bin/pylightshow.py
+++ bin/pylightshow.py
@@ -3,6 +3,7 @@
 import numpy as np
 import sounddevice as sd
 from queue import Queue, Empty
+import time
 
 
 # Global Variables:
@@ -13,15 +14,22 @@ queue = Queue()
 def audio_callback(indata, frames, time, status):
     if status:
         print(status, flush=True)
+    print(type(indata))
     queue.put(indata)
 
 # Audio in object
-audio_stream = sd.InputStream(channels=1, samplerate=44100.0, callback=audio_callback)
+audio_stream = sd.InputStream(channels=1, samplerate=44100.0, callback=audio_callback, blocksize=1024)
 
 
 # All calculations are performed here
 def update():
-    audio_buf = queue
+    # get audio out of queue
+    try:
+        audio_buf = queue.get(block=True)
+    except Empty:
+        pass
+    # process
+
 
 
 # Setup is done in here
