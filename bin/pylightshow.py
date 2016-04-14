4a53344a4c7f4ad2401938991c4cfd08fc41c6cd
Denzo77 <denzo1@gmail.com>
2016-04-14 16:08:28 +0100

    Started implementing sound stream in

--- bin/pylightshow.py
+++ bin/pylightshow.py
@@ -1,3 +1,44 @@
 #!/usr/bin/env python3
 
-print("hello")
+import numpy as np
+import sounddevice as sd
+from queue import Queue, Empty
+
+
+# Global Variables:
+queue = Queue()
+
+
+# This is called from another thread once audio buffer is filled
+def audio_callback(indata, frames, time, status):
+    if status:
+        print(status, flush=True)
+    queue.put(indata)
+
+# Audio in object
+audio_stream = sd.InputStream(channels=1, samplerate=44100.0, callback=audio_callback)
+
+
+# All calculations are performed here
+def update():
+    audio_buf = queue
+
+
+# Setup is done in here
+def setup():
+    audio_stream.start()
+
+
+# Stuff to do when exiting
+def end_program():
+    audio_stream.close()
+
+
+# Main loop
+def main():
+    while True:
+        update()
+
+
+if __name__ == "__main__":
+    main()
