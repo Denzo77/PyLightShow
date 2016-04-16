a8fb91472f249e9ba950718c798b6440445cc52b
Denzo77 <denzo1@gmail.com>
2016-04-16 14:03:03 +0100

    Basic beat detection with bar output working

--- pylightshow/pylightshow.py
+++ pylightshow/pylightshow.py
@@ -5,6 +5,19 @@ import numpy as np
 from queue import Empty
 import pygame
 
+# GUI
+pygame.init()
+clock = pygame.time.Clock()
+size = width, height = 1024, 768
+screen = pygame.display.set_mode(size)
+COLOUR_BACKGROUND = (40, 40, 40)
+COLOUR_VOL_INSTANT = (150, 150, 150)
+COLOUR_VOL_AVERAGE = (0, 0, 0)
+
+bar_width = 40
+bar_vol_instant = pygame.Rect((width-bar_width)/2, height-38, bar_width, 1)
+bar_vol_average = pygame.Rect((width-bar_width)/2 + 2, height-40, bar_width-4, 1)
+
 
 def update():
     """
@@ -24,15 +37,25 @@ def update():
     return ms
 
 
+def display(vol_instant, vol_average):
+    height_instant = int(vol_instant * 2000)
+    height_average = int(vol_average * 2000)
+    bar_vol_instant.height = -height_instant
+    bar_vol_average.height = -height_average
+    pygame.draw.rect(screen, COLOUR_VOL_INSTANT, bar_vol_instant)
+    pygame.draw.rect(screen, COLOUR_VOL_AVERAGE, bar_vol_average)
+
+
 def main():  # This is the main loop
-    b = BasicBeatDetect(sensitivity_grad=0.0, sensitivity_offset=1.0, cutoff=0.001)
+    b = BasicBeatDetect(sensitivity_grad=-2.0e-8, sensitivity_offset=1.01, cutoff=0.001)
     while True:
         val = update()
-        b.update(val)
-        if b.get():
-            print('i')
-        else:
-            print('o')
+        if val is not None:
+            b.update(val)
+        screen.fill(COLOUR_BACKGROUND)
+        display(b.vol_instant, b.vol_average)
+        pygame.display.flip()
+        clock.tick(60)
 
 
 if __name__ == "__main__":
