a316c877a606a8d32a612669fddee900f7608274
Denzo77 <denzo1@gmail.com>
2016-04-16 13:23:05 +0100

    beatdetect working

--- pylightshow/pylightshow.py
+++ pylightshow/pylightshow.py
@@ -1,7 +1,9 @@
 #!/usr/bin/env python3
 import sound_input as sound
+from pybeatdetect import BasicBeatDetect
 import numpy as np
 from queue import Empty
+import pygame
 
 
 def update():
@@ -17,16 +19,20 @@ def update():
         pass
     # process (temporarily calculating RMS)
     audio_buf = np.power(audio_buf, 2)
-    ms = np.mean(audio_buf)
-    if ms > 0.1:
-        print("1")
-    else:
-        print("o")
+    audio_buf = np.mean(audio_buf)
+    ms = np.sqrt(audio_buf)
+    return ms
 
 
 def main():  # This is the main loop
+    b = BasicBeatDetect(sensitivity_grad=0.0, sensitivity_offset=1.0, cutoff=0.001)
     while True:
-        update()
+        val = update()
+        b.update(val)
+        if b.get():
+            print('i')
+        else:
+            print('o')
 
 
 if __name__ == "__main__":
