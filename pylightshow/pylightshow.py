13691cab5388bd347d8627f78afc4b9b581f91cd
Denzo77 <denzo1@gmail.com>
2016-04-17 13:06:59 +0100

    Fixed problem with FFT

--- pylightshow/pylightshow.py
+++ pylightshow/pylightshow.py
@@ -17,15 +17,16 @@ COLOUR_BACKGROUND = (30, 30, 30)
 bar_width = 40
 
 
-def update():
+def update(block):
     """
     Calculations are done in this. Intended for future abstraction.
     Currently has a test function.
     :return: RMS value of audio_buf. Doubled to scale to +-1
     """
     # get audio out of queue
+    audio_buf = np.zeros(512)
     try:
-        audio_buf = sound.queue.get(block=True)
+        audio_buf = sound.queue.get(block=block)
     except Empty:
         pass
     # # process (temporarily calculating RMS)
@@ -38,10 +39,10 @@ def update():
 
 def draw_val(indata):
     indata[:] = indata[:] * 2.0e1
-    x = np.arange(100, 100+len(indata))
-    offset = height/2
+    x = np.arange(100, 100+20*len(indata), 20)
+    offset = height-40
     for i in range(len(indata)):
-        pygame.draw.line(screen, (100, 100, 100), (x[i], offset), (x[i], offset-int(indata[i])), 1)
+        pygame.draw.line(screen, (100, 100, 100), (x[i], offset), (x[i], offset-int(indata[i])), 18)
 
 
 def main():  # This is the main loop
@@ -49,20 +50,22 @@ def main():  # This is the main loop
     b = GuiBeatDetect(average_weight=0.1, sensitivity_grad=-2.0e-3, sensitivity_offset=1.4, cutoff=0.001,
                         left=(width-bar_width)/2, top=40, width=bar_width, height=700)
     # b = SimpleBeatDetect(average_weight=0.1, sensitivity_grad=-2.0e-2, sensitivity_offset=1.4, cutoff=0.001)
+    block = True
     while True:
-        val = update()
+        val = update(block)
+        block = False
+        b.update(val)
         if val is not None:
-            b.update(val)
             screen.fill(COLOUR_BACKGROUND)
             # b.draw(screen)
             draw_val(val)
-            pygame.display.flip()
+        pygame.display.flip()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sound.stream.stop()
                 pygame.quit()
                 sys.exit
-        # clock.tick(60)
+        clock.tick(60)
 
 
 if __name__ == "__main__":
--- pylightshow/sound_input.py
+++ pylightshow/sound_input.py
@@ -1,19 +1,21 @@
 import sounddevice as sd
 from queue import Queue
 import numpy as np
-from scipy import fftpack
+from scipy import fftpack, signal
 
 
 """
 Handle details of audio input stream.
 """
-DEVICE = None
+DEVICE = 4
 CHANNELS = 1
 SAMPLERATE = 44100.0
 BLOCKSIZE = 1024
 DTYPE = None
 
 queue = Queue()  # For passing audio between files
+
+window = signal.hamming(BLOCKSIZE, False)
 
 
 def audio_callback(indata, frames, time, status):
@@ -27,16 +29,18 @@ def audio_callback(indata, frames, time, status):
     if status:
         print(status, flush=True)
     if any(indata):
-        outdata = np.abs(fftpack.fft(indata))
-    # outdata = np.power(fftpack.rfft(indata), 2)
-    # outdata = np.zeros(10)
-    # for i in range(10):
-    #     index1 = 2**i
-    #     index2 = 2**(i+1)
-    #     outdata[i] = np.sum(indata[index1:index2]) / (index2-index1)
-    #     # outdata[i] = np.sum(indata[2**i:2**(i+1)])
-    # queue.put(outdata)
-        queue.put(indata)
+        # outdata = np.abs(fftpack.fft(indata)[:512, 0])
+        # outdata = np.zeros(BLOCKSIZE/2)
+        temp = np.abs(fftpack.rfft((indata[:, 0] * window), overwrite_x=True))
+        temp = np.power(temp, 2)
+
+        outdata = np.zeros(10)
+        for i in range(10):
+            index1 = 2**i
+            index2 = 2**(i+1)
+            outdata[i] = np.sum(temp[index1:index2]) / (index2-index1)
+            # outdata[i] = np.sum(indata[2**i:2**(i+1)])
+            queue.put(outdata)
 
 stream = sd.InputStream(device=DEVICE,
                         channels=CHANNELS,               # Set up the audio stream
@@ -47,20 +51,3 @@ stream = sd.InputStream(device=DEVICE,
 
 
 
-# import numpy as np
-# import matplotlib.pyplot as plt
-# import scipy.fftpack
-#
-# # Number of samplepoints
-# N = 600
-# # sample spacing
-# T = 1.0 / 800.0
-# x = np.linspace(0.0, N*T, N)
-# y = np.sin(50.0 * 2.0*np.pi*x)
-# plt.plot(x, y)
-# yf = scipy.fftpack.rfft(y)
-# xf = np.linspace(0.0, 1.0/(2.0*T), N/2)
-#
-# fig, ax = plt.subplots()
-# ax.plot(xf, 2.0/N * np.abs(yf[:N/2]))
-# plt.show()
