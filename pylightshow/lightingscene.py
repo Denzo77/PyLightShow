b1f9c8598e64acfa70db7a6b0d2d9ba5c523d36c
Denzo77 <denzo1@gmail.com>
2016-04-18 22:22:17 +0100

    Started on scenes

---
+++ pylightshow/lightingscene.py
@@ -0,0 +1,55 @@
+"""
+Framework for managing what the lights do.
+
+Random Notes:
+- All scenes need:
+    - buffers in, e.g. audio, midi, osc.
+    - buffers out to send stuff out, e.g. serial, dmx512, osc, midi.
+- Scenes can share:
+    - Lights.
+    - Beat onset detectors.
+    - GUI elements.
+    - Sound streams.
+- Each scene needs its own:
+    - Name.
+    - Setup method.
+    - Update method.
+    - Draw method.
+    - Exit method.
+"""
+
+
+class BaseScene:
+    def __init__(self, name):
+        self.next = self
+        self.name = name
+
+    def process_input(self, events):
+        print("You forgot to overide")
+
+    def update(self):
+        print("You forgot to overide")
+
+    def draw(self, screen):
+        print("You forgot to overide")
+
+    def switch_to_scene(self, next_scene):
+        self.next = next_scene
+
+
+class LightScene(BaseScene):  # class myClass(baseClass): means myClass inherits from baseClass
+    def __init__(self, name, beats, lights):
+        super().__init__(name)
+        self.beats = beats
+        self.lights = lights
+
+    # def process_input(self, events):
+
+    def update(self):
+        self.beats.update()
+        self.lights.update()
+
+    def draw(self, screen):
+        self.beats.draw()
+        self.lights.draw()
+
