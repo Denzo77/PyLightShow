cf569cc6184bee21ea1bcb67d035d5b0aab9e487
Denzo77 <denzo1@gmail.com>
2016-04-18 20:28:05 +0100

    vectorizing light arrays

--- pylightshow/pylights.py
+++ pylightshow/pylights.py
@@ -96,55 +96,55 @@ class SingleLight(BaseLight):
                          (self.position, self.size))
 
 
-class MultiLight(BaseLight):
-    """
-    Light object that supports a multiple channels (i.e. RGB).
-    WARNING: Although this class can take channel values other than 3, this may cause errors if draw() is called.
-    """
-    def __init__(self, name, position, size, channels=3):
-        """
-
-        :param name: Identifier for this light. Gets printed below the light.
-        :param position: Position of top left corner as tuple (left, top).
-        :param size: Size of the light as tuple (width, height).
-        :param channels: Number of channels the light has. Defaults to 3 for RGB.
-        """
-        super().__init__(name, position, size)
-        self.value_target = np.zeros(channels)
-        self.value_current = np.zeros(channels)
-        self.value_range = self.value_min, self.value_max = np.zeros(channels), np.ones(channels)
-        self.damping = 0.3
-        self.value_output = np.zeros(channels, dtype=np.int)
-
-    def set(self, value, damped=True):
-        """
-        Set value to change to.
-        This only sets the internal targets. value_output is only calculated after update() is called.
-        :param value: Values between 0.0 and 1.0 to set light to.
-        :type value: numpy float array of size channels.
-        :param damped: Whether to smooth the change. If False, the light will immediately jump to the new value when
-        update() is called.
-        :return: None
-        """
-        self.value_target = np.clip(value, self.value_min, self.value_max)
-        if not damped:
-            self.value_current = np.clip(value, self.value_min, self.value_max)
-
-    def update(self):
-        """
-        Updates value_output and value_current.
-        value_current is updated after value_output is set so it's new value will not be seen until update() is next
-        called.
-        :return: None
-        """
-        self.value_output = (self.value_current * 255.0).astype(int)
-        self.value_current = self.value_current + self.damping * (self.value_target - self.value_current)
-
-    def draw(self, surface):
-        """
-        Draws the light onto a surface
-        WARNING: Do not call this if channels is not 3.
-        :param surface: The surface to draw the light onto.
-        :return: None
-        """
-        pygame.draw.rect(surface, self.value_output, (self.position, self.size))
+# class MultiLight(BaseLight):
+#     """
+#     Light object that supports a multiple channels (i.e. RGB).
+#     WARNING: Although this class can take channel values other than 3, this may cause errors if draw() is called.
+#     """
+#     def __init__(self, name, position, size, channels=3):
+#         """
+#
+#         :param name: Identifier for this light. Gets printed below the light.
+#         :param position: Position of top left corner as tuple (left, top).
+#         :param size: Size of the light as tuple (width, height).
+#         :param channels: Number of channels the light has. Defaults to 3 for RGB.
+#         """
+#         super().__init__(name, position, size)
+#         self.value_target = np.zeros(channels)
+#         self.value_current = np.zeros(channels)
+#         self.value_range = self.value_min, self.value_max = np.zeros(channels), np.ones(channels)
+#         self.damping = 0.3
+#         self.value_output = np.zeros(channels, dtype=np.int)
+#
+#     def set(self, value, damped=True):
+#         """
+#         Set value to change to.
+#         This only sets the internal targets. value_output is only calculated after update() is called.
+#         :param value: Values between 0.0 and 1.0 to set light to.
+#         :type value: numpy float array of size channels.
+#         :param damped: Whether to smooth the change. If False, the light will immediately jump to the new value when
+#         update() is called.
+#         :return: None
+#         """
+#         self.value_target = np.clip(value, self.value_min, self.value_max)
+#         if not damped:
+#             self.value_current = np.clip(value, self.value_min, self.value_max)
+#
+#     def update(self):
+#         """
+#         Updates value_output and value_current.
+#         value_current is updated after value_output is set so it's new value will not be seen until update() is next
+#         called.
+#         :return: None
+#         """
+#         self.value_output = (self.value_current * 255.0).astype(int)
+#         self.value_current = self.value_current + self.damping * (self.value_target - self.value_current)
+#
+#     def draw(self, surface):
+#         """
+#         Draws the light onto a surface
+#         WARNING: Do not call this if channels is not 3.
+#         :param surface: The surface to draw the light onto.
+#         :return: None
+#         """
+#         pygame.draw.rect(surface, self.value_output, (self.position, self.size))
--- pylightshow/pylightshow.py
+++ pylightshow/pylightshow.py
@@ -14,11 +14,21 @@ import json
 # GUI
 pygame.init()
 clock = pygame.time.Clock()
-size = width, height = 1600, 768
+size = width, height = 800, 768
 screen = pygame.display.set_mode(size, pygame.HWSURFACE)
 COLOUR_BACKGROUND = (30, 30, 30)
 
 bar_width = 40
+
+# beats and lights
+b = PlotBeatDetect(average_weight=0.3, sensitivity_grad=-2.0e-4, sensitivity_offset=1.4, cutoff=0.001,
+                   position=(100, 40), size=(400, 700))
+# light = pylights.SingleLight('flashy', (600, 50), (100, 100))
+# light2 = pylights.SingleLight('bass', (600, 200), (100, 100))
+# light3 = pylights.MultiLight('colour', (600, 350), (100, 100))
+lights = [pylights.SingleLight('many ' + str(x), (600, 50 + x * 150), (100, 100)) for x in range(4)]
+print(lights[0].name, end='\t')
+print(lights[0].position)
 
 
 def save_state():
@@ -45,6 +55,7 @@ def save_state():
     """
     pass
 
+
 def load_state():
     """
     Load state.
@@ -58,6 +69,7 @@ def load_state():
         - Set data in relevant places.
     Otherwise, start up with default values.
     """
+    pass
 
 
 def update_audio(block):
@@ -66,47 +78,46 @@ def update_audio(block):
     :return: Array of sound values or None if queue is empty.
     """
     try:
-        audio_buf = sound.queue.get(block=block)
-        return audio_buf
+        b.update(sound.queue.get(block=block))
+        # temp1 = b.dBFS(b.vol_average[1]) * 0.01 + 1.0
+        # temp2 = b.dBFS(b.vol_average[3]) * 0.01 + 1.0
+        # temp3 = b.dBFS(b.vol_average[6]) * 0.01 + 1.0
+        # light2.set(max(temp1 + 1.0, 0.0))
+        # temparray = np.maximum(np.array([temp1, temp2, temp3]), 0.0)
+        # light3.set(temparray)
+        if b.beat[1]:
+            [lights[x].set(1.0, False) for x in range(len(lights))]
     except Empty:
         return None
 
 
 def main():  # This is the main loop
     sound.stream.start()
-    b = PlotBeatDetect(average_weight=0.3, sensitivity_grad=-2.0e-4, sensitivity_offset=1.4, cutoff=0.001,
-                       position=(100, 40), size=(400, 700))
-    light = pylights.SingleLight('flashy', (800, 50), (100, 100))
-    light2 = pylights.SingleLight('bass', (800, 200), (100, 100))
-    light3 = pylights.MultiLight('colour', (800, 350), (100, 100))
+
     block = True
     draw_frame = True
     while True:
+        # Only update stuff that depends on audio if buffer in queue.
+        update_audio(block)
+        # We want these to run independent of sound input (lights carry on fading even if stream is interrupted)
+        # light.update()
+        # light2.update()
+        # light3.update()
+        # light.set(0.0)
+        [lights[x].update() for x in range(len(lights))]
+        [lights[x].set(0.0) for x in range(len(lights))]
+        block = False
+        # Only draw every other frame (30 Hz) to reduce artifacts.
         draw_frame = not draw_frame
-        val = update_audio(block)
-        if val is not None:  # TODO This entire statement can go into update_audio
-            block = False
-            b.update(val)
-            temp1 = b.dBFS(b.vol_average[1]) * 0.01 + 1.0
-            temp2 = b.dBFS(b.vol_average[3]) * 0.01 + 1.0
-            temp3 = b.dBFS(b.vol_average[6]) * 0.01 + 1.0
-            light2.set(max(temp1+1.0, 0.0))
-            temparray = np.maximum(np.array([temp1, temp2, temp3]), 0.0)
-            light3.set(temparray)
-            if b.beat[1]:
-                light.set(1.0, False)
-        light.update()
-        light2.update()
-        light3.update()
-        light.set(0.0)
         if draw_frame:
             screen.fill(COLOUR_BACKGROUND)
             b.draw(screen)
-            light.draw(screen)
-            light2.draw(screen)
-            light3.draw(screen)
+            [lights[x].draw(screen) for x in range(len(lights))]
+            # light.draw(screen)
+            # light2.draw(screen)
+            # light3.draw(screen)
             pygame.display.flip()
-
+        # Events loop.
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sound.stream.stop()
