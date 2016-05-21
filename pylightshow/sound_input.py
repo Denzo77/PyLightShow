24a3a6fbf6a32fc8abb80113672b2e961b31c158
Deniz Erbilgin <denzo@denzo-desktop>
2016-05-21 15:12:44 +0100

    removed scipy dependency

--- pylightshow/sound_input.py
+++ pylightshow/sound_input.py
@@ -6,10 +6,10 @@ Could use numpy.fft.rfft and include the hamming window in here to save on impor
 import sounddevice as sd
 from queue import Queue
 import numpy as np
-from scipy import fftpack, signal
+# from scipy import fftpack, signal
 
 
-DEVICE = 4
+DEVICE = None
 CHANNELS = 1
 SAMPLERATE = 44100.0
 BLOCKSIZE = 1024  # gives us a minimum frequency of ~43 Hz
@@ -17,7 +17,7 @@ DTYPE = None  # this defaults to float32
 
 queue = Queue()  # For passing audio between files
 
-window = signal.hamming(BLOCKSIZE, False)  # Hamming window for the FFT to give us a sane plot.
+window = np.hamming(BLOCKSIZE)  # Hamming window for the FFT to give us a sane plot.
 
 
 def audio_callback(indata, frames, time, status):
@@ -35,7 +35,7 @@ def audio_callback(indata, frames, time, status):
     if status:
         print(status, flush=True)
     if any(indata):  # not sure if this is needed.
-        temp = np.abs(fftpack.rfft((indata[:, 0] * window), overwrite_x=True))  # window data, get FFT find absolute.
+        temp = np.abs(np.fft.rfft((indata[:, 0] * window)))  # window data, get FFT find absolute.
         temp = np.power(temp, 2)  # We are interested in signal power so we square it.
         outdata = np.zeros(10)
         for i in range(10):  # Binning happens here.
